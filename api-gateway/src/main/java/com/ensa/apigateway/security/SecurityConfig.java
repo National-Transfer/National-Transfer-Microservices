ee5e4eb2722266727a58de890552117f1873d4d4
Yahya Boujrah <yahyaboujrah@Yahyas-MacBook-Air.local>
2023-12-26 19:55:48 +0100

    add okta config

---
+++ api-gateway/src/main/java/com/ensa/apigateway/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ensa.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(authz -> {
+                    authz.anyExchange().authenticated();
+
+                }).oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
