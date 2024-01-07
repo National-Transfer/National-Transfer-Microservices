63df3a5b4396a2e4f8af4c93bebc71fb8df679fa
reda <reda.himmi@edu.uca.ma>
2024-01-07 19:57:02 +0100

    changed cors config

--- api-gateway/src/main/java/com/ensa/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/ensa/apigateway/security/SecurityConfig.java
@@ -1,23 +1,44 @@
-package com.ensa.apigateway.security;
+//package com.ensa.apigateway.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-        http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchangeSpec -> exchangeSpec
-                        .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-    }
-}
+import java.util.Collections;
+
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//
+//    @Bean
+//    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+//        http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .cors(Customizer.withDefaults())
+//                .authorizeExchange(exchangeSpec -> exchangeSpec
+//                        .anyExchange().authenticated()
+//                )
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+//        return http.build();
+//    }
+//}
