af80be7dd6a57376dc19456ead954888600a3e38
reda <reda.himmi@edu.uca.ma>
2024-01-08 23:35:57 +0100

    added ci file

--- account-service/src/test/java/com/ensa/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/ensa/accountservice/AccountServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ensa.accountservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AccountServiceApplicationTests {
 
 	@Test
--- agency-service/src/test/java/com/ensa/agencyservice/SalesPointEntityServiceApplicationTests.java
+++ agency-service/src/test/java/com/ensa/agencyservice/SalesPointEntityServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ensa.agencyservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SalesPointEntityServiceApplicationTests {
 
 	@Test
